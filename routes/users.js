2c6e62098ccbdcb25651691858f1ccf62cb82672
Jesse Maingi <maingijesse@gmail.com>
2019-12-26 11:39:53 +0300

    added reg forms and fixed overflow issue

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,34 @@ const router = express.Router();
 
 //user login
 router.get("/login", (req, res) => {
-  res.send("successfully logged in");
+  res.render("users/login");
 });
 
 //user register
 router.get("/register", (req, res) => {
-  res.send("Register");
+  res.render("users/register");
+});
+
+router.post("/register", (req, res) => {
+  let errors = [];
+  if (req.body.password != req.body.confirmPassword) {
+    errors.push({ text: "Passwords do not match" });
+  }
+
+  if (req.body.password.length < 5) {
+    errors.push({ text: "Password must be at least 4 characters" });
+  }
+  if (errors.length > 0) {
+    res.render("users/register", {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword
+    });
+  } else {
+    res.send("passed");
+  }
 });
 
 module.exports = router;
